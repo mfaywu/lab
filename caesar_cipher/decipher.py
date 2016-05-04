6132ca6e2df162b80c31973d6b42f38f9a43d064
M. Fay Wu <mfaywu@ucla.edu>
2016-05-04 11:02:19 -0700

    Added decipher for Caesar cipher

--- caesar_cipher/decipher.py
+++ caesar_cipher/decipher.py
@@ -5,7 +5,13 @@ def decipher(ciphertext):
         plaintext = ''
         for j in range(0, len(ciphertext)):
             if ciphertext[j].isalpha():
-                plaintext += 
+                pos = ord(ciphertext[j])
+                new_pos = (pos - 97 + 26 - i) % 26 + 97
+                plaintext += unichr(new_pos)
+            else:
+                plaintext += ciphertext[j]
+        print("%i: %s" % (i, plaintext))
+    return
 
 ciphertext = input ("Enter ciphertext: ")
 print (decipher(ciphertext))
