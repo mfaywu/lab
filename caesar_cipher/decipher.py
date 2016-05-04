3e18945738353eebd56bd7acbf65d574cddf98a5
M. Fay Wu <mfaywu@ucla.edu>
2016-05-04 14:53:49 -0700

    Add decipher solves key

--- caesar_cipher/decipher.py
+++ caesar_cipher/decipher.py
@@ -1,17 +1,35 @@
+import enchant
+from enchant.checker import SpellChecker
+
 MAX_KEY = 26
 
 def decipher(ciphertext):
+    checker = SpellChecker("en_US")
+    best_key = -1
+    least_num_errors = len(ciphertext)
     for i in range(0, 26):
         plaintext = ''
         for j in range(0, len(ciphertext)):
             if ciphertext[j].isalpha():
                 pos = ord(ciphertext[j])
-                new_pos = (pos - 97 + 26 - i) % 26 + 97
+                offset = 97
+                if ciphertext[j].isupper():
+                    offset = 65
+                new_pos = (pos - offset + 26 - i) % 26 + offset
                 plaintext += unichr(new_pos)
             else:
                 plaintext += ciphertext[j]
-        print("%i: %s" % (i, plaintext))
-    return
+        checker.set_text(plaintext)
+        num_errors = 0
+        for err in checker:
+            num_errors = num_errors + 1
+        if num_errors < least_num_errors:
+            least_num_errors = num_errors
+            best_key = i
+        words = plaintext.split()
+        en_words = len(words) - num_errors
+        print("%i: %s English words: %i" % (i, plaintext, en_words))
+    return "%s %i" % ("The key is most likely: ", best_key)
 
 ciphertext = input ("Enter ciphertext: ")
 print (decipher(ciphertext))
--- caesar_cipher/make_ciphertext.py
+++ caesar_cipher/make_ciphertext.py
@@ -6,7 +6,10 @@ def encrypt (plaintext, key):
         if i.isalpha():
             #from 97 to 122
             pos = ord(i)
-            new_pos = (pos - 97 + key) % MAX_KEY + 97
+            offset = 97
+            if i.isupper(): 
+                offset = 65
+            new_pos = (pos - offset + key) % MAX_KEY + offset
             translated += unichr(new_pos)
         else:
             translated += i
@@ -14,4 +17,6 @@ def encrypt (plaintext, key):
 
 plaintext = input ("Enter plain text: ")
 key = input ("Enter key [0,26]: ")
+while key > 27 or key < 0:
+    key = input ("Enter a valid key [0, 26]: ")
 print (encrypt(plaintext, key))
